3ac7bdc6202ca03d2bf46ce116fe84c08e307da6
Annie Hsieh <ming.ting@gmail.com>
2013-09-08 22:12:54 -0500

    add message board tests

--- train_spec.rb
+++ train_spec.rb
@@ -2,12 +2,22 @@ require 'rspec'
 require './train'
 describe Conductor do
 
-	let(:engineer) { Engineer.new }
-	let(:conductor) { Conductor.new(engineer)}
+	let(:conductor) { Conductor.new(message_board) }
+	let(:message_board) { mock }
 
-	it "should tell the engineer to slow down" do
-		engineer.should_receive(:slow_down!)
+	it "should tell the message board to slow down" do
+		message_board.should_receive(:slow_down!)
 		conductor.see_danger_coming!
 	end
 	
+end
+
+describe Engineer do
+	let(:engineer) { Engineer.new(message_board) }
+	let(:message_board) { mock }
+
+	it "should send message board a slow down confirmation" do
+		message_board.should_receive(:confirm_slow_down)
+		engineer.slow_down
+	end
 end
