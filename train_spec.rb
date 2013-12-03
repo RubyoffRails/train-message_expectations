1b77e57d84d08ae2b3c5fbca096ad909083959e9
mizuhands <mizuhands@gmail.com>
2013-12-03 08:09:09 -0800

    Update the scope of class instances without including Train module

--- train_spec.rb
+++ train_spec.rb
@@ -2,12 +2,10 @@
 require 'rspec'
 require './train'
 
-include Train
-
 describe "Train" do
 	let(:mockboard) { double('messageboard') }
-	let(:conductor) { Conductor.new(mockboard) }
-	let(:engineer)  { Engineer.new(mockboard) }
+	let(:conductor) { Train::Conductor.new(mockboard) }
+	let(:engineer)  { Train::Engineer.new(mockboard) }
 
 	it "receives a 'slow down' message when the conductor sees trouble" do
 		mockboard.should_receive(:slow_down!)
