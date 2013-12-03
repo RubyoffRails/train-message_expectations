983bac46dcfec73af7fd13bceccd18ebeb755b95
mizuhands <mizuhands@gmail.com>
2013-12-03 00:26:27 -0800

    Update train.rb and train_spec.rb for PR

--- train_spec.rb
+++ train_spec.rb
@@ -1,13 +1,21 @@
+# train_spec.rb
 require 'rspec'
 require './train'
-describe Conductor do
 
-	let(:engineer) { Engineer.new }
-	let(:conductor) { Conductor.new(engineer)}
+include Train
 
-	it "should tell the engineer to slow down" do
-		engineer.should_receive(:slow_down!)
-		conductor.see_danger_coming!
+describe "Train" do
+	let(:mockboard) { double('messageboard') }
+	let(:conductor) { Conductor.new(mockboard) }
+	let(:engineer)  { Engineer.new(mockboard) }
+
+	it "receives a 'slow down' message when the conductor sees trouble" do
+		mockboard.should_receive(:slow_down!)
+		conductor.sees_danger_coming!
 	end
-	
+
+	it "recieves a 'confirmation' message when engineer slows down" do
+		mockboard.should_receive(:confirm_slow_down)
+		engineer.slows_down
+	end
 end
