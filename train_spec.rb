c8e8694e51d440ebe1ca230b92253f1f1fb0483e
ThePomodoro <danny@itjournalist.com>
2014-01-18 16:10:50 -0800

    Messageboard added

--- train_spec.rb
+++ train_spec.rb
@@ -2,12 +2,18 @@ require 'rspec'
 require './train'
 describe Conductor do
 
-	let(:engineer) { Engineer.new }
-	let(:conductor) { Conductor.new(engineer)}
+	let(:engineer) { Engineer.new(messageboard) }
+	let(:conductor) { Conductor.new(engineer, messageboard)}
+  let(:messageboard) {double}
 
-	it "should tell the engineer to slow down" do
-		engineer.should_receive(:slow_down!)
+	it "should tell the message board to slow down" do
+		messageboard.should_receive(:slow_down!)
 		conductor.see_danger_coming!
-	end
+  end
+
+  it "should confirm slow down to messageboard" do
+    messageboard.should_receive(:confirm_slow_down)
+    engineer.confirm_slow_down
+  end
 	
 end
