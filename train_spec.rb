6ad7230c59dec63ded6fad280e179a185e56153c
drammopo <drammopo@gmail.com>
2014-02-09 22:32:06 +0200

    Feature: Initial attempt at the exercise.

--- train_spec.rb
+++ train_spec.rb
@@ -2,12 +2,26 @@ require 'rspec'
 require './train'
 describe Conductor do
 
-	let(:engineer) { Engineer.new }
-	let(:conductor) { Conductor.new(engineer)}
+	let(:message_board) { MessageBoard.new()}
+  let(:engineer) { Engineer.new (message_board)}
+  let(:conductor) { Conductor.new(message_board,engineer)}
 
-	it "should tell the engineer to slow down" do
-		engineer.should_receive(:slow_down!)
-		conductor.see_danger_coming!
-	end
-	
+  it "should see trouble and tell the `message_board` to slow down" do
+    engineer.should_receive(:slow_down!)
+    conductor.see_danger_coming!
+  end
+end
+
+describe Engineer do
+
+  let(:message_board) { MessageBoard.new()}
+  let(:engineer) { Engineer.new (message_board)}
+  let(:conductor) { Conductor.new(message_board,engineer)}
+
+  it "slows down and tells the `message_board` `confirm_slow_down`" do
+    engineer.should_receive(:slow_down!)
+    conductor.see_danger_coming!
+    message_board.should_receive(:confirm_slow_down)
+    engineer.avert_danger!
+  end
 end
