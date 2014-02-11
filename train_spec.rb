3c456e1a6cc13b807f5c9bf5c1cf26bd96cc6367
drammopo <drammopo@gmail.com>
2014-02-11 21:16:50 +0200

    Changed specification block to reflect 1 assertion per block.

--- train_spec.rb
+++ train_spec.rb
@@ -6,7 +6,7 @@ describe Conductor do
   let(:engineer) { Engineer.new (message_board)}
   let(:conductor) { Conductor.new(message_board,engineer)}
 
-  it "should see trouble and tell the `message_board` to slow down" do
+  it "sees trouble and tells the engineer to slow down via the message board" do
     engineer.should_receive(:slow_down!)
     conductor.see_danger_coming!
   end
@@ -18,9 +18,12 @@ describe Engineer do
   let(:engineer) { Engineer.new (message_board)}
   let(:conductor) { Conductor.new(message_board,engineer)}
 
-  it "slows down and tells the `message_board` `confirm_slow_down`" do
+  it "receives the slows down message" do
     engineer.should_receive(:slow_down!)
     conductor.see_danger_coming!
+  end
+
+  it "replies back with the confirmation message via the message board" do
     message_board.should_receive(:confirm_slow_down)
     engineer.avert_danger!
   end
