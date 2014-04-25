3f8f811a6a5051015f6cc9fb545ee5ddbe632cc8
PDaily <pat.irwin4@gmail.com>
2014-04-25 22:35:08 +0000

    Added messageboard functionality

--- train.rb
+++ train.rb
@@ -1,15 +1,35 @@
 class Conductor
 
-	attr_reader :engineer
+  attr_reader :engineer
+	attr_reader :message_board
 
-	def initialize(engineer)
-		@engineer = engineer
+	def initialize(message_board, engineer)
+    @engineer = engineer
+		@message_board = message_board
 	end
 
 	def see_danger_coming!
-		engineer.slow_down!
+		message_board.message(engineer.slow_down!)
 	end
 end
 
 class Engineer
+
+  attr_reader :message_board
+
+  def initialize(message_board)
+    @message_board = message_board
+  end
+
+  def confirm!
+    message_board.confirm_slow_down!
+  end
+end
+
+class MessageBoard
+
+  def message(msg)
+    msg
+  end
+
 end
--- train_spec.rb
+++ train_spec.rb
@@ -2,12 +2,29 @@ require 'rspec'
 require './train'
 describe Conductor do
 
-	let(:engineer) { Engineer.new }
-	let(:conductor) { Conductor.new(engineer)}
+  let(:engineer) { Engineer.new (message_board)}
+  let(:conductor) { Conductor.new(message_board,engineer)}
+  let(:message_board) { MessageBoard.new }
 
-	it "should tell the engineer to slow down" do
-		engineer.should_receive(:slow_down!)
-		conductor.see_danger_coming!
-	end
-	
+  it "Should notify the messageboard to slow down when danger is imminent" do
+    engineer.should_receive(:slow_down!)
+    conductor.see_danger_coming!
+  end
+end
+
+describe Engineer do
+
+  let(:engineer) { Engineer.new (message_board)}
+  let(:conductor) { Conductor.new(message_board,engineer)}
+  let(:message_board) { MessageBoard.new }
+
+  it "Should receive the slowdown message" do
+    engineer.should_receive(:slow_down!)
+    conductor.see_danger_coming!
+  end
+
+  it "Should reply to the Message Board with a slow down confirmation" do
+    message_board.should_receive(:confirm_slow_down!)
+    engineer.confirm!
+  end
 end
