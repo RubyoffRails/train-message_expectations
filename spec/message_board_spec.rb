308d7dc822cd5200a646288aedf3963c9a326d42
Adam Lowe <tidalinfluence@gmail.com>
2014-05-24 09:24:39 -0700

    Added MessageBoard class; conductor passes slow_down to message_board; engineer passes confirm_slow_down to message_board

--- spec/message_board_spec.rb
+++ spec/message_board_spec.rb
@@ -6,10 +6,10 @@ describe MessageBoard do
 	let(:engineer) { Engineer.new(message_board) }
 	let(:conductor) { Conductor.new(message_board)}
 
-	it "should tell the engineer to slow down" do
-		message_board.should_receive(:slow_down!)
-		conductor.see_danger_coming!
-	end
+  it "should tell the engineer to slow down" do
+    message_board.should_receive(:slow_down!)
+	  conductor.see_danger_coming!
+  end
 
   it "should confirm to the conductor that it slowed down" do
     message_board.should_receive(:confirm_slow_down)
--- train_spec.rb
+++ train_spec.rb
@@ -2,12 +2,24 @@ require 'rspec'
 require './train'
 describe Conductor do
 
-	let(:engineer) { Engineer.new }
-	let(:conductor) { Conductor.new(engineer)}
+	let(:engineer) { mock }
+	let(:message_board) { mock }
+	let(:conductor) { Conductor.new(engineer, message_board)}
 
-	it "should tell the engineer to slow down" do
-		engineer.should_receive(:slow_down!)
+	it "should tell the message board to slow down" do
+		message_board.should_receive(:slow_down)
 		conductor.see_danger_coming!
 	end
-	
+end
+
+describe Engineer do
+
+	let(:message_board) { mock }
+	let(:engineer) { Engineer.new(message_board) }
+
+
+	it "should tell the message board to confirm slow down" do
+		message_board.should_receive(:confirm_slow_down)
+		engineer.slow_down
+	end
 end
